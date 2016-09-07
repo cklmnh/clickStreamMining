7def3faad020d09ce54bc6b4d8caedd5384705d2
John Kuhn <johnkuhn@Johns-MacBook-Pro.local>
2016-09-06 21:42:16 -0400

    updated mining.py

--- mining.py
+++ mining.py
@@ -1,3 +1,10 @@
-line = "John is so cool"
+line = "D00-MON 02:07:24 U16329 Z1678 U02390 y"#this is an example line, will eventually grab files to fill 2d array
 
-print(line)
+info = []
+info.append(line.split(' '))
+info.append(line.split(' '))
+
+time = info[0][1]
+
+print(info)
+print(time)
