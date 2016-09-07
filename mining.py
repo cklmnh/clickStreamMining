e70a2e36d9115a30e42f779453dca6b7b275f2a7
John Kuhn <johnkuhn@Johns-MacBook-Pro.local>
2016-09-07 10:53:16 -0400

    reads in all files now

--- mining.py
+++ mining.py
@@ -1,11 +1,15 @@
-fileDirectory = "Webscope_G5/ydata-ymessenger-user-communication-pattern-v1_0/ydata-ymessenger-user-communication-pattern-v1_0-d00.txt"
-
-file = open(fileDirectory, "r")#open file for reading based off of directory above
+fileDirectory = "Webscope_G5/ydata-ymessenger-user-communication-pattern-v1_0/ydata-ymessenger-user-communication-pattern-v1_0-d"
 
 info = []#2d array for info
 
-for line in file:
-    info.append(line.split(' '))
+for x in range(0, 28): #there are 28 total files needed to read in
+    if x < 10:
+        file = open(fileDirectory + "0" + repr(x) + ".txt", "r")# open file for reading based off of directory above
+    else:
+        file = open(fileDirectory + repr(x) + ".txt", "r")
 
-print(info[5])
-print(len(info))
+    for line in file:
+        info.append(line.split(' '))
+
+
+#print info[5]
